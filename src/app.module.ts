599a158bf9d53c4c1da17b9f192c697b782f4843
Menescal <lexdmm@protonmail.com>
2022-07-27 15:39:59 -0300

    add swagger api doc

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { join } from 'path'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { FormModule } from './form/form.module'
 
 @Module({
@@ -29,8 +27,6 @@ import { FormModule } from './form/form.module'
             }
         }),
         FormModule
-    ],
-    controllers: [AppController],
-    providers: [AppService]
+    ]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,27 @@ import {
     FastifyAdapter,
     NestFastifyApplication
 } from '@nestjs/platform-fastify'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
+import { FormModule } from './form/form.module'
 
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(
         AppModule,
         new FastifyAdapter()
     )
+
+    const options = new DocumentBuilder()
+        .setTitle('Form API')
+        .setDescription('API responsible for creating dynamic system forms')
+        .setVersion('1.0')
+        .addTag('api/doc/form')
+        .build()
+    const formDocument = SwaggerModule.createDocument(app, options, {
+        include: [FormModule]
+    })
+    SwaggerModule.setup('api/doc/form', app, formDocument)
+
     await app.listen(3000, '0.0.0.0')
 }
 bootstrap()
